9be25a96398916014418bfc99d4af71b77089391
shamilh2o <shamil.prematunga@h2o.ai>
2023-08-26 22:59:39 +0530

    Add material ui tables

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -4,6 +4,7 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Table from '../Table';
 
 function CustomTabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -49,13 +50,13 @@ export default function SignUp() {
     <Box sx={{ width: '100%' }}>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
         <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-          <Tab label="Item One" {...a11yProps(0)} />
+          <Tab label="Material UI Tables" {...a11yProps(0)} />
           <Tab label="Item Two" {...a11yProps(1)} />
           <Tab label="Item Three" {...a11yProps(2)} />
         </Tabs>
       </Box>
       <CustomTabPanel value={value} index={0}>
-        Item One
+        <Table />
       </CustomTabPanel>
       <CustomTabPanel value={value} index={1}>
         Item Two
